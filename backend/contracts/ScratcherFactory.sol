19f1b9242f0eaaf1182056ac61e242e5fcdd626f
mikegerst <mike.gerst87@gmail.com>
2024-02-29 21:54:13 -0700

    -Added first draft ScratcherFactory Contract

---
+++ backend/contracts/ScratcherFactory.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.17;
+import "./Scratcher.sol";
+
+contract ScratcherFractory {
+    uint nextScratcherId = 0;
+
+    struct ScratcherInfo {
+        uint id;
+        address scratcherAddress;
+        string name;
+    }
+    mapping(uint => ScratcherInfo) scratcherAddresses;
+    event ScartcherCreated(string _name, address _address);
+    
+    function createScratcher(uint64 chainlinkVRFSubscriptionId, 
+                string memory name, 
+                string memory symbol,
+                uint price,
+                uint totalForSale,
+                uint64 maxPurchase)public returns(address){
+                    
+                    Scratcher scratcher = new Scratcher(chainlinkVRFSubscriptionId, name, symbol, price, totalForSale, maxPurchase, msg.sender);
+                    address scratcherAddress = address(scratcher);
+                    scratcherAddresses[nextScratcherId] = ScratcherInfo(nextScratcherId, scratcherAddress, name); 
+                    nextScratcherId++;
+                    emit ScartcherCreated(name, scratcherAddress);
+                    return scratcherAddress;
+                }
+
+}
