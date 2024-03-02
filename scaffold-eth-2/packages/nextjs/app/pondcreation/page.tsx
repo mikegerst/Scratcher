36279bf813eb6cd003293589e9ae96d320eea66f
davidthegardens <monsterunits@gmail.com>
2024-03-02 04:34:14 +0000

    hide frogs

--- scaffold-eth-2/packages/nextjs/app/pondcreation/page.tsx
+++ scaffold-eth-2/packages/nextjs/app/pondcreation/page.tsx
@@ -4,8 +4,7 @@ import PondCreator from '../../components/PondCreator';
 const pondcreation: React.FC = () => {
   
   return (
-    <div>
-      <h1 className="">Let's get brewin'</h1>
+    <div className="bg-lime-50">
       <PondCreator />
     </div>
   );
--- scaffold-eth-2/packages/nextjs/components/PondCreator.tsx
+++ scaffold-eth-2/packages/nextjs/components/PondCreator.tsx
@@ -24,30 +24,34 @@ const PondCreator: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex p-40 bg-lime-50">
+    <div>
+    <h1 className="flex justify-center pb-20 pt-20 text-center text-black text-vw-8 break-words">Let's get brewin'</h1>
+    <div className="flex pl-40 pr-40 pb-40 bg-lime-50 items-center justify-center">
       <div className="w-1/2">
         {frogUrl &&
-        <img src={frogUrl} alt="Square" className="w-70 h-auto object-cover rounded-lg item-center" />}
+        <img src={frogUrl} alt="Square" className="w-30 h-auto object-cover rounded-lg item-center" />}
       </div>
-      <div className="w-1/2 flex flex-col justify-between">
+      <div className="rounded-lg border-2 border-grey w-300 h-300 flex-col items-center justify-center overflow-hidden">
+      <div className="w-fill h-auto m-20 flex flex-col justify-between">
         {!isPondCreated ? (
           <>
             {/* Before Pond Creation */}
             <div>
-              <label htmlFor="winRate" className="block text-black">Win rate distribution (1-256)</label>
+              <label htmlFor="winRate" className="block text-black font-bold">Win rate distribution (1-256)</label>
               <input type="text" id="winRate" className="mt-1 mb-4 p-2 border rounded" />
 
-              <label htmlFor="expiryDate" className="block text-black">Expiry Date</label>
+              <label htmlFor="expiryDate" className="block text-black font-bold">Expiry Date</label>
               <input type="date" id="expiryDate" className="mt-1 mb-4 p-2 border rounded" />
-            </div>
-            <button onClick={handleCreatePond} className="bg-secondary text-white p-2 rounded">
+            
+            <button onClick={handleCreatePond} className="block bg-lime-500 text-white p-2 rounded mt-1">
               Create Pond
             </button>
+            </div>
           </>
         ) : !isPondFunded ? (
           <>
             {/* After Pond Creation but before funding */}
-            <div className="flex flex-col">
+            <div className="">
               <h2 className="text-lg font-bold mb-4 text-black">Fill your pond with otherworldly delights</h2>
               <label htmlFor="tokenAddress" className="block text-black">Token Address</label>
               <input type="text" id="tokenAddress" className="mt-1 mb-4 p-2 border rounded" />
@@ -55,7 +59,7 @@ const PondCreator: React.FC = () => {
               <label htmlFor="amount" className="block text-black">Amount (decimal adjusted)</label>
               <input type="text" id="amount" className="mt-1 mb-4 p-2 border rounded" />
             </div>
-            <button onClick={handleFundPond} className="bg-green-500 text-white p-2 rounded">
+            <button onClick={handleFundPond} className="block bg-lime-500 text-white p-2 rounded mt-1">
               Fund Pond
             </button>
           </>
@@ -64,6 +68,8 @@ const PondCreator: React.FC = () => {
           <div className="text-8xl font-bold text-black text-break p-4">Godspeed, frogman.</div>
         )}
       </div>
+    </div>
+    </div>
     </div>
   );
 };
